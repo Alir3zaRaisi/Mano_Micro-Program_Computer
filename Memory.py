9e9411d2ae01fc12c40c4d116a15ec071850e8ca
Alir3zaRaisi <95176495+Alir3zaRaisi@users.noreply.github.com>
2023-06-30 10:25:09 +0330

    Added lookup function

--- Memory.py
+++ Memory.py
@@ -6,12 +6,42 @@ class Memory:
         self.registers = []
         for i in range(length):
             self.registers.append(Register(bit, f'{i}'))
+        self.table = {}
 
     def read(self, index):
         return self.registers[index].binary
 
     def write(self, index, value):
         self.registers[index].set(value)
+
+    def lookup(self, lines):
+        lc = 0
+        num = ["HEX", "DEC", "BIN"]
+        for line in lines:
+            if line[:3] == "ORG":
+                line.replace(' ', '')
+                lc = int(line[3:])
+                lc = lc - 1
+            elif ':' in line:
+                operand = line.split(':')
+                value = 0
+                if operand[1][1:4] == "HEX":
+                    value = int(operand[1][5:], 16)
+                elif operand[1][1:4] == "BIN":
+                    value = int(operand[1][5:], 2)
+                else:
+                    value = int(operand[1][5:], 10)
+                self.table[operand[0]] = [lc, value]
+                self.write(lc, value)
+            elif line[:3] in num:
+                if line[:3] == "HEX":
+                    value = int(line[4:], 16)
+                elif line[:3] == "DEC":
+                    value = int(line[4:])
+                else:
+                    value = int(line[4:], 2)
+                self.write(lc, value)
+            lc += 1
 
 
 Basic_Memory = Memory(2048, 16)
